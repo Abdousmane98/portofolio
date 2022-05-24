d5dae19cf7a0ede3c1742c50ebe31ab29e0b54ef
Abdousmane98 <abdousmane98@gmail.com>
2022-05-24 12:52:25 +0200

    modifier

---
+++ css/script.js
@@ -0,0 +1,10 @@
+// variables
+let mainaccount= 'abdou';
+let othersaccount= 'ousmane';
+
+let temp= mainaccount;
+mainaccount= othersaccount;
+othersaccount= temp;
+
+console.log('mainaccount');
+console.log('othersaccount');
