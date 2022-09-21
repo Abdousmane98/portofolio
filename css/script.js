801b04c7a985cbf3ae42a0bf931fe683463f5e2f
Abdousmane98 <abdousmane98@gmail.com>
2022-09-21 22:05:15 +0200

    new

---
+++ css/script.js
@@ -0,0 +1,9 @@
+(function () {
+    alert ('Bienvenue')
+})
+();
+
+let sendButton=document.getElementById ('send')
+sendButton=addEventListener ('click', function (){
+    alert('vous etes sure de vouloir envoyez?')
+})
